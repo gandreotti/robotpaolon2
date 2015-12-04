f8d8a596c9cb150c0b57f5e4b8cbb7aded9db8dd
Geovanni Andreotti <gandreotti@udd.cl>
2015-12-04 16:36:57 -0300

    delete test.py

--- core/test.py
+++
@@ -1,9 +0,0 @@
-import random
-with open('piropos.txt') as f:
-    lines = f.readlines()
-    print random.choice(lines)
-
-#with random.choice(open('piropos.txt')) as f:
-#	line = f.readlines()
-#	print line
-
